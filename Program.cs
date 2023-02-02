ddcd79088be8f41671a2ba78936b46dadb385fa0
Alex <122726581+Alex6706@users.noreply.github.com>
2023-02-02 19:00:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,43 @@
-﻿//Задача 37
-// Найдите произведение пар чисел в одномерном массиве. 
-//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-//Результат запишите в новом массиве.
+﻿// Задача 37 (Вариант 2) 
+//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний
+// элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 
-Console.WriteLine("Задача 37: Найдите произведение пар чисел в одномерном массиве.");
-Console.WriteLine("Парой считаем первый и последний элемент, второй и предпоследний и т.д.");
+using System;
 
-int N = 10;
-int[] array = new int[N];
-Console.Write("Массив" + " : ");
+const int size_N=16;
 
-for (int i = 0; i < array.Length; i++)
+int [] array = new int[size_N];
+
+void print_array(int [] local_array)
 {
-    array[i] = new Random().Next(0, 10);
-    Console.Write(array[i] + " ");
-}
-Console.WriteLine();
-Console.Write("Произведение пар чисел" + " : ");
+Console.Write("[");
+for(int i=0; i < local_array.Length-1; i++)
+     {
+         Console.Write($"{local_array[i]}, ");
+     }
+     Console.WriteLine($"{local_array[local_array.Length-1]}]");
+ }
 
-int j = array.Length - 1;
-for (int i = 0; i < (array.Length + 1) / 2; i++)
-{
-    Console.Write(array[i] * array[j] + " ");
-    j--;
-}
+ void init_array()
+ {
+     Random rnd = new Random();
+     for(int i=0; i<size_N; i++)
+     {
+         array[i]=rnd.Next(-10,11);
+     }
+ }
+
+ int [] process_array2(int [] local_array)
+ {
+   int [] ret_array = new int[local_array.Length/2];
+   for (int i=0; i<ret_array.Length;i++)
+   {
+       ret_array[i] = local_array[i] * local_array[local_array.Length - i -1];
+   }
+   return ret_array;
+ }
+
+ init_array();
+ print_array(array);
+ int [] array1 = process_array2(array);
+ print_array(array1);
